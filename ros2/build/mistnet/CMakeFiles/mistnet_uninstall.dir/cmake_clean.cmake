649afed32f94e4b65b6011af972e5edbea38f84e
Orin-Xavier <xavierxuan@foxmail.com>
2024-06-19 16:55:56 +0800

    remove temps

--- ros2/build/mistnet/CMakeFiles/mistnet_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mistnet_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mistnet_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros2/build/mistnet/CMakeFiles/mistnet_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for mistnet_uninstall.
